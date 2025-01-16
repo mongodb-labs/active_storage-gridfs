b9de8ab8c8f2c89fef1920e1499db56cda8217ca
Darshana Venkatesh <darshana.venkatesh@mongodb.com>
2025-01-16 10:56:16 -0800

    initialize gem

---
+++ lib/active_storage/gridfs.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "gridfs/version"
+
+module ActiveStorage
+  module Gridfs
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
